01227e8917cf3fd5107790f396738977364d7573
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-02-13 16:00:53 +0600

    updated ellipse JS

--- js/ellips.js
+++ js/ellips.js
@@ -1,29 +1,6 @@
 function calculateAreaOfEllipse(){
-    //get ellipse a
-    const ellipseAInput = document.getElementById('ellipse-a');
-    const ellipseAInputText = ellipseAInput.value;
-    const ellipseAInputValue = parseFloat(ellipseAInputText);
-
-    //get ellipse b
-    const ellipseBInput = document.getElementById('ellipse-b');
-    const ellipseBInputText = ellipseBInput.value;
-    const ellipseBInputValue = parseFloat(ellipseBInputText);
-
-    //calculate the area 
-    const area = 3.1416 * ellipseAInputValue * ellipseBInputValue;
-
-    //display the output
-    const ellipseAreaSpan = document.getElementById('ellipse-area');
-    ellipseAreaSpan.innerText = area;
+    const majorRadius =getInputValueById('ellipse-a');
+    const minorRadius = getInputValueById('ellipse-b');
+    const area = 3.1416 * majorRadius * minorRadius;
+    setInnerTextById('ellipse-area', area);
 }
-
-
-/**
- * 1. get base value
- * 2. added an ID
- * 3. use getElementById to access the input field
- * 4. get value from the input field (value is string)
- * 5. convert the value to a number by using pareFloat
- * 6. calculate the area
- * 7. output the result by using innerText
- */
